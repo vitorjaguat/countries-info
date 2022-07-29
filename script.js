a7600c315b803de771d44f6e886ba0276e353adc
Vitor Jaguat <vitorbutkus@gmail.com>
2022-07-29 19:39:02 -0300

    Correct Brazils neighbours, removing France, which was there because of French Guyana

--- script.js
+++ script.js
@@ -134,12 +134,127 @@ const renderCountry = function (data, className = '') {
 
 
 //All neighbour countries:
+// const getCountryData = function (country) {
+//     //Country 1:
+//     fetch(`https://restcountries.com/v2/name/${country}`)
+//         .then(response => response.json())
+//         .then(data => {
+//             renderCountry(data[0]);
+//             const neighbour = data[0].borders;
+//             console.log(neighbour)
+//             if (!neighbour) return;
+
+//             //Country 2:
+//             setTimeout(() => neighbour.forEach(neiCou => {
+//                 return fetch(`https://restcountries.com/v2/alpha/${neiCou}`)
+
+//                     .then(response => response.json())
+//                     .then(data => renderCountry(data, 'neighbour'))
+//                     .catch(err => {
+//                         console.error(`${err} 💥`);
+//                         alert(`${err} 💥💥💥`)
+//                     })
+//                     .finally(() => {
+//                         countriesContainer.style.opacity = 1;
+//                     })
+//             }), 200)
+//         })
+// };
+
+
+// btn.addEventListener('click', function () {
+//     getCountryData('iceland')
+// })
+
+// In this challenge you will build a function 'whereAmI' which renders a country only based on GPS coordinates. For that, you will use a second API to geocode coordinates. So in this challenge, you’ll use an API on your own for the first time 😁
+// Your tasks:
+// PART 1
+// 1. Create a function 'whereAmI' which takes as inputs a latitude value ('lat') and a longitude value ('lng') (these are GPS coordinates, examples are in test data below).
+// 2. Do “reverse geocoding” of the provided coordinates. Reverse geocoding means to convert coordinates to a meaningful location, like a city and country name. Use this API to do reverse geocoding: https://geocode.xyz/api. The AJAX call will be done to a URL with this format: https://geocode.xyz/52.508,13.381?geoit=json. Use the fetch API and promises to get the data. Do not use the 'getJSON' function we created, that is cheating 😉
+// 3. Once you have the data, take a look at it in the console to see all the attributes that you received about the provided location. Then, using this data, log a message like this to the console: “You are in Berlin, Germany”
+// 4. Chain a .catch method to the end of the promise chain and log errors to the console
+// 5. This API allows you to make only 3 requests per second. If you reload fast, you will get this error with code 403. This is an error with the request. Remember, fetch() does not reject the promise in this case. So create an error to reject the promise yourself, with a meaningful error message
+// PART 2
+// 6. Now it's time to use the received data to render a country. So take the relevant attribute from the geocoding API result, and plug it into the countries API that we have been using.
+// 7. Render the country and catch any errors,  just like we have done in the last lecture (you can even copy this code, no need to type the same code)
+// Test data:
+// § Coordinates 1: 52.508, 13.381 (Latitude, Longitude) 
+// § Coordinates 2: 19.037, 72.873
+// § Coordinates 3: -33.933, 18.474
+
+// const [coords1, coords2, coords3] = [[52.508, 13.381], [19.037, 72.873], [-33.933, 18.474]]
+
+// const whereAmI = function (coords) {
+//     const [lat, lgn] = coords;
+//     fetch(`https://geocode.xyz/${lat},${lgn}?geoit=json`)
+//         .then(response => response.json())
+//         .then(data => {
+//             console.log(`You are in ${data.city}, ${data.country}`);
+//             getCountryData(data.country)
+//         })
+//         .catch(err => {
+//             console.log(`😿 Error: ${err}`)
+//             countriesContainer.insertAdjacentText('beforeend', `😿 Something went wrong: ${err}`)
+//         })
+// }
+
+// whereAmI(coords3)
+
+// const getCountryData = function (country) {
+//     //Country 1:
+//     fetch(`https://restcountries.com/v2/name/${country}`)
+//         .then(response => response.json())
+//         .then(data => {
+//             renderCountry(data[0]);
+//             const neighbour = data[0].borders;
+//             console.log(neighbour)
+//             if (!neighbour) return;
+
+//             //Country 2:
+//             setTimeout(() => neighbour.forEach(neiCou => {
+//                 return fetch(`https://restcountries.com/v2/alpha/${neiCou}`)
+
+//                     .then(response => response.json())
+//                     .then(data => renderCountry(data, 'neighbour'))
+//                     .catch(err => {
+//                         console.error(`${err} 💥`);
+//                         alert(`${err} 💥💥💥`)
+//                     })
+//                     .finally(() => {
+//                         countriesContainer.style.opacity = 1;
+//                     })
+//             }), 200)
+//         })
+// };
+
+
+//Using getCurrentPosition and displaying neighbour countries:
+const [coords1, coords2, coords3] = [[52.508, 13.381], [19.037, 72.873], [-33.933, 18.474]]
+
+const whereAmI = function (coords) {
+    const [lat, lgn] = coords;
+    fetch(`https://geocode.xyz/${lat},${lgn}?geoit=json`)
+        .then(response => response.json())
+        .then(data => {
+            console.log(`You are in ${data.city}, ${data.country}`);
+            getCountryData(data.country)
+        })
+        .catch(err => {
+            console.log(`😿 Error: ${err}`)
+            countriesContainer.insertAdjacentText('beforeend', `😿 Something went wrong: ${err}`)
+        })
+}
+
+// whereAmI(coords3)
+
 const getCountryData = function (country) {
     //Country 1:
     fetch(`https://restcountries.com/v2/name/${country}`)
         .then(response => response.json())
         .then(data => {
             renderCountry(data[0]);
+            console.log(data[0])
+            if (data[0].name === 'Brazil') data[0].borders.splice(3, 1);
             const neighbour = data[0].borders;
             console.log(neighbour)
             if (!neighbour) return;
@@ -161,8 +276,17 @@ const getCountryData = function (country) {
         })
 };
 
-
 btn.addEventListener('click', function () {
-    getCountryData('iceland')
-})
+    navigator.geolocation.getCurrentPosition(pos => {
+        const { latitude: lat, longitude: lgn } = pos.coords;
+        whereAmI([lat, lgn])
+        console.log(lat, lgn);
+        countriesContainer.insertAdjacentHTML('afterbegin', '<div class="country extra"><p>🪐</p> <p></p><p>You are here and your neighbours are:</p><div>');
+        btn.remove();
 
+
+    })
+
+
+    // getCountryData()
+})
